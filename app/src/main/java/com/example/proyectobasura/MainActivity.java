f6d14cb2c8c7137724054cbf15ea1e71a583cb5b
Mathias <m.cabrera1@outlook.com>
2019-06-23 15:46:12 -0400

    Fix permisos de ubicacion y validacion GPS activado

--- app/src/main/java/com/example/proyectobasura/MainActivity.java
+++ app/src/main/java/com/example/proyectobasura/MainActivity.java
@@ -1,53 +1,64 @@
 package com.example.proyectobasura;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean permisoUbicacion = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Verificar permisos de posición
+        obtenerPermisosUbicacion();
     }
 
     protected void onClick(View view){
         Intent intento = null;
+        final LocationManager locationManager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
 
         switch (view.getId()){
             case R.id.btnRecolector:
-                intento = new Intent(MainActivity.this,MapsActivity.class);
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    alertaGpsDesactivado();
+                }else {
+                    intento = new Intent(MainActivity.this,MapsActivity.class);
+                }
                 break;
 
             case R.id.btnCiudadano:
-                final LocationManager locationManager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
-
                 if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                     alertaGpsDesactivado();
                 }else {
                     intento = new Intent(MainActivity.this, CiudadanoActivity.class);
-                }
-                break;
+                }break;
 
         }
         try{
             startActivity(intento);
-            }
+        }
         catch (Exception e) {
 
-            }
+        }
     }
 
     /*
@@ -70,5 +81,23 @@ public class MainActivity extends AppCompatActivity {
                 });
         final AlertDialog alert = builder.create();
         alert.show();
+    }
+    /*
+    Obtener permisos de ubicacion
+    Verifica si los permisos para acceder al GPS están concedidos
+    sino los pide.
+    */
+    private void obtenerPermisosUbicacion() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                permisoUbicacion = true;
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1234);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, 1234);
+        }
     }
 }
